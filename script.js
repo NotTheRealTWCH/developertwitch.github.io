cc96052fe3f2ecd13d25dd16968fb470375fe051
Twitch <ianletwitch@gmail.com>
2017-08-15 04:18:50 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// SET LANDING PAGE MAX HEIGHT
+const editorPane = document.getElementById('backgroundCodeEditor__pane');
+const editorLine = document.getElementsByClassName('backgroundCodeEditor__pane__line');
+const landingPage = document.getElementById('landingPage');
+const header = document.getElementById('header-C');
+
+let browserWidth = window.innerWidth;
+let paneHeight;
+let panePadding;
+let extraSpace;
+
+function setMaxHeight() {
+	paneHeight = editorLine[0].clientHeight * 40;
+	panePadding = window.innerWidth * 1.25 / 100 * 40 / 100;
+	extraSpace = editorPane.offsetHeight - paneHeight - panePadding;
+	
+	if (extraSpace => 94.6) {
+		let maxHeight = landingPage.offsetHeight - extraSpace + 94.6 + "px";
+		landingPage.style.maxHeight = maxHeight;
+	}
+}
+
+setMaxHeight();
+
+window.addEventListener('resize', function() {
+	if (window.innerWidth !== browserWidth) {
+		setMaxHeight();
+	}
+});
