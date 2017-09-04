ba09b324d009090e1ed1de38a398cc4e7f3fa8c6
Twitch <ianletwitch@gmail.com>
2017-09-04 12:24:14 +1000

    Update script.js

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ messageInput.addEventListener('blur', function () {
 
 // FORMSPREE FORM AJAX POST
 let request = new XMLHttpRequest();
-requestion.open('POST', 'https://formspree.io/ian@developetwitch.com', true);
+request.open('POST', 'https://formspree.io/ian@developetwitch.com', true);
 request.setRequestHeader('accept', 'application/json');
 
 const form = document.forms[0];
